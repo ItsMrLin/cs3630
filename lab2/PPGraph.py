8f7c2d5451a4f746a6e40e9c4fb6caabcf3b11f0
Zhen Liu <liuzhen1994@hotmail.com>
2016-02-21 20:21:11 -0500

    bugs on PPGraph fixed

--- lab2/PPGraph.py
+++ lab2/PPGraph.py
@@ -1,9 +1,10 @@
+import os
 import csv
 import numpy as np
-from myro import *
+# from myro import *
 
 class PPGraph:
-	def __init__(filename):
+	def __init__(self, filename):
 		self.node = {}
 		self.adjMatrix = None
 		self.nodeToInd = {}
@@ -11,18 +12,19 @@ class PPGraph:
 			raise "path not exists"
 		with open(filename, 'r') as ppMap:
 			mapReader = csv.reader(ppMap)
-			mode = 0
+			mode = 0 # mode 0 for reading coordinate; mode 1 for reading edges of graph
 			count = 0
 			for row in mapReader:
 				if len(row) == 0:
 					mode = 1
 					self.adjMatrix = np.zeros((count, count))
+					continue
 				if mode == 0:
 					self.node[row[0]] = (int(row[1]), int(row[2]))
 					self.nodeToInd[row[0]] = count
 					count += 1
 				if mode == 1:
-					first = self.nodeToInd[int(row[0])]
-					sec = self.nodeToInd[int(row[1])]
+					first = self.nodeToInd[row[0]]
+					sec = self.nodeToInd[row[1]]
 					self.adjMatrix[first][sec] = 1
 					self.adjMatrix[sec][first] = 1
