6272ea14d5ef814d90ae0457a043ea850690604d
ItsMrLin <JerryLin1206@Gmail.com>
2016-03-12 18:36:22 -0500

    set up main structure

---
+++ lab3/lab3.py
@@ -0,0 +1,17 @@
+from myro import *
+
+def mainLogic():
+
+
+def main():
+	f = open('../robot_name.conf', 'r')
+	init(f.readline())
+	f.close()
+	try:
+		mainLogic()
+		# turnBy(90, "deg")
+	finally:
+		stop()
+
+if __name__ == "__main__":
+	main()
