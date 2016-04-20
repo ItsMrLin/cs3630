normalization fixed

lab4/lab4.py
@@ -90,7 +90,7 @@ def update(particles, weights, keypoints):
     for i in xrange(particles.shape[0]):
       distances = np.apply_along_axis(np.linalg.norm, 0, np.array(map(lambda x: x.pt, keypoints)))
       weights[i] = np.min(distances)
-    weights /= np.max(weights)
+    weights /= np.sum(weights)
 
     return particles, weights
 