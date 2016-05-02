87fdb1be8d236b6fcfec9f28cfab450d673a3eed
ItsMrLin <JerryLin1206@Gmail.com>
2016-05-02 19:02:40 -0400

    indentation

--- final/random_move.py
+++ final/random_move.py
@@ -3,13 +3,13 @@ import random
 
 def mainLogic():
 	while True:
-        # p = 0.3
-        # with p probility making a random turn
-        if random.random() < 0.5:
-            turnBy(int(random.random() * 360), 'deg')
-        else:
-            # with 1-p probability moving forward 1 - 3 seconds at half speed
-            forward(0.5, 1 + random.random() * 2)
+		# p = 0.3
+		# with p probility making a random turn
+		if random.random() < 0.5:
+			turnBy(int(random.random() * 360), 'deg')
+		else:
+			# with 1-p probability moving forward 1 - 3 seconds at half speed
+			forward(0.5, 1 + random.random() * 2)
 
 def main():
 	f = open('../robot_name.conf', 'r')
